99454fdf0d95d60d165a648ee1299974b2dd1e84
yhermimejiamt <91927432+yhermimejiamt@users.noreply.github.com>
2021-12-05 18:08:26 -0500

    Solucion Frontend Bike

--- src/main/java/com/usa/ciclo4/hackathon/controller/IndexController.java
+++ src/main/java/com/usa/ciclo4/hackathon/controller/IndexController.java
@@ -21,62 +21,69 @@ import com.usa.ciclo4.hackathon.service.UserService;
 
 @Controller
 public class IndexController {
-	@Autowired
-	private UserService service;
 
-	@GetMapping("/")
-	public String index() {
-		return "vistas/index";
-	}
+    @Autowired
+    private UserService service;
 
-	@GetMapping("/login")
-	public String login() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
-			return "vistas/login";
-		}
-		return "redirect:/home";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "vistas/index";
+    }
 
-	@GetMapping("/register")
-	public String register(Model model) {
-		Map<String, String> genders = new LinkedHashMap<String, String>();
-		genders.put("M", "Hombre");
-		genders.put("F", "Mujer");
-		model.addAttribute("genders", genders);
-		model.addAttribute("user", new User());
-		return "vistas/register";
-	}
+    @GetMapping("/login")
+    public String login() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "vistas/login";
+        }
+        return "redirect:/home";
+    }
 
-	@GetMapping("/home")
-	public String homePage() {
-		return "vistas/home";
-	}
+    @GetMapping("/register")
+    public String register(Model model) {
+        Map<String, String> genders = new LinkedHashMap<String, String>();
+        genders.put("M", "Hombre");
+        genders.put("F", "Mujer");
+        model.addAttribute("genders", genders);
+        model.addAttribute("user", new User());
+        return "vistas/register";
+    }
 
-	@PostMapping(path = "/register", consumes = "application/x-www-form-urlencoded")
-	public String validaRegistro(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
+    @GetMapping("/home")
+    public String homePage() {
+        return "vistas/home";
+    }
 
-		if (result.hasErrors()) {
-			model.addAttribute("user", user);
-			Map<String, String> genders = new LinkedHashMap<String, String>();
-			genders.put("M", "Hombre");
-			genders.put("F", "Mujer");
-			model.addAttribute("genders", genders);
-			return "vistas/register";
-		} else {
-			try {
-				service.createUser(user);
-			} catch (Exception e) {
-				Map<String, String> genders = new LinkedHashMap<String, String>();
-				genders.put("M", "Hombre");
-				genders.put("F", "Mujer");
-				model.addAttribute("genders", genders);
-				model.addAttribute("user", user);
-				model.addAttribute("validationError", e.getMessage());
-				return "vistas/register";
-			}
-		}
-		model.addAttribute("registerSuccess", true);
-		return "vistas/login";
-	}
+    @GetMapping("/bike")
+    public String bike() {
+        return "vistas/bike";
+    }
+
+    @PostMapping(path = "/register", consumes = "application/x-www-form-urlencoded")
+    public String validaRegistro(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("user", user);
+            Map<String, String> genders = new LinkedHashMap<String, String>();
+            genders.put("M", "Hombre");
+            genders.put("F", "Mujer");
+            model.addAttribute("genders", genders);
+            return "vistas/register";
+        } else {
+            try {
+                service.createUser(user);
+            } catch (Exception e) {
+                Map<String, String> genders = new LinkedHashMap<String, String>();
+                genders.put("M", "Hombre");
+                genders.put("F", "Mujer");
+                model.addAttribute("genders", genders);
+                model.addAttribute("user", user);
+                model.addAttribute("validationError", e.getMessage());
+                return "vistas/register";
+            }
+        }
+        model.addAttribute("registerSuccess", true);
+        return "vistas/login";
+    }
+
 }
